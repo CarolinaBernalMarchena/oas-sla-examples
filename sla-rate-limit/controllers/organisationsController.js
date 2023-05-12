156f1f8434bddde3d0803b10207981a64a25f2f8
Carolina Bernal <carolinabm2000@gmail.com>
2023-05-12 16:19:32 +0200

    examples finished

---
+++ sla-rate-limit/controllers/organisationsController.js
@@ -0,0 +1,8 @@
+export function getOrganisations(_, res) {
+    res.send({ "registry_number": 14, "professional_org": "FARMACÉUTICOS DE CÁDIZ" })
+}
+
+export function findByid(_, res) {
+    res.send({ "registry_number": 14, "professional_org": "FARMACÉUTICOS DE CÁDIZ" })
+}
+
--- sla-rate-limit/showcase/slatest.js
+++ sla-rate-limit/showcase/slatest.js
@@ -1,22 +1,22 @@
 import { run } from "apipecker";
 import jwt from "jsonwebtoken";
 
-function myUrlBuilder(){
-    return "http://localhost:8080/api/v1/teams";
+function myUrlBuilder() {
+    return "http://localhost:8080/api/v1/organisations";
 }
 
-function myRequestBuilder(user){
-    const token = jwt.sign({plan: "base", apikey: user}, process.env.JWT_SECRET, {issuer: process.env.JWT_ISSUER});
-    
+function myRequestBuilder(user) {
+    const token = jwt.sign({ plan: "base", apikey: user }, process.env.JWT_SECRET, { issuer: process.env.JWT_ISSUER });
+
     return {
-        options : {
+        options: {
             method: "GET",
             headers: { 'Authorization': `Bearer ${token}` }
         }
     }
 }
 
-function myResultsHandler(results){
+function myResultsHandler(results) {
     console.log("Test result:");
     console.log(JSON.stringify(results.summary, null, 2));
     console.log(JSON.stringify(results.lotStats?.flatMap(o => o.result.stats.map(s => `Iteration: ${o.id} - User: ${s.id} => Response Status: ${s.statusCode}`)), null, 2));
@@ -24,11 +24,11 @@ function myResultsHandler(results){
 }
 
 run({
-    concurrentUsers : 1,
-    iterations : 21,
-    delay : 20,
-    verbose : true,
+    concurrentUsers: 1,
+    iterations: 21,
+    delay: 20,
+    verbose: true,
     urlBuilder: myUrlBuilder,
-    requestBuilder : myRequestBuilder,
-    resultsHandler : myResultsHandler
+    requestBuilder: myRequestBuilder,
+    resultsHandler: myResultsHandler
 });
